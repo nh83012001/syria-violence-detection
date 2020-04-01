e3f7a08946e2d111c920c38fa069138523ab5653
Nick Hall <nh83012001@gmail.com>
2020-04-01 19:52:40 +0100

    updates to cypher scripts

--- cypher-scripts.js
+++ cypher-scripts.js
@@ -14,6 +14,7 @@ ON CREATE SET tweet.text = status.text, tweet.created_at = status.created_at, tw
 MERGE(user: User { name: u.screen_name })
 ON CREATE SET user.location = u.location, user.id = u.id, user.lang = u.lang, user.verified = u.verified, user.geo_enabled = u.geo_enabled, user.created_at = u.created_at, user.time_zone = u.time_zone, user.statuses_count = u.statuses_count
 MERGE(user) - [: POSTED] -> (tweet)
+ 
 
 // add in all the entities the user mentions in the tweet
 FOREACH(mention IN e.user_mentions | MERGE(mentionedUser: User { name: mention.screen_name }) MERGE(tweet) - [: MENTIONED_USER] -> (mentionedUser))
@@ -21,12 +22,13 @@ FOREACH(hashtag IN e.hashtags | MERGE(mentionedHashtag: Hashtag { text: hashtag.
 FOREACH(symbol IN e.symbols | MERGE(mentionedSymbol: Symbol { text: symbol.text }) MERGE(tweet) - [: MENTIONED_SYMBOL] -> (mentionedSymbol))
 FOREACH(url IN e.urls | MERGE(mentionedUrl: Url { url: url.url }) MERGE(tweet) - [: MENTIONED_URL] -> (mentionedUrl))
 FOREACH(media IN e.media | MERGE(mentionedMedia: Media { media_url: media.media_url, type: media.type }) MERGE(tweet) - [: MENTIONED_MEDIA] -> (mentionedMedia))
-
+// TODO Do a count for anythign with :Keyword labels words to see how many times it is in the content
 
 // // ************Script for loading keyword hierarchy
 CALL apoc.load.json("file://keywords.json") YIELD value
 UNWIND value.keywordHierarchy as subject
 WITH subject
 CALL apoc.create.node([subject.node], { words: subject.words }) YIELD node
+SET node:Keywords
 RETURN subject
 
